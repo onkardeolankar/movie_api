fb6ab38350bc3a82c01a84df5a18ae9126aee16c
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-08-21 00:36:39 +0530

    once updated again

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -6,47 +6,56 @@ const SingleMovie = () => {
 	const { id } = useParams();
 	const [isLoading, setIsLoading] = useState(true);
 	const [movie, setMovie] = useState("");
-	const [isError, setIsError] = useState("");
+	const [isError, setIsError] = useState(true);
 
-	const getMovies= async(url)=>{
+	const getMovies = async (url) => {
 		setIsLoading(true);
-		try{
+		try {
 			const res = await fetch(url);
 			const data = await res.json();
 			console.log(data);
 			if (data.Response === "True") {
 				setIsLoading(false);
 				setMovie(data);
-			} else {
-				setIsError({
-					show: true,
-					msg: data.Error,
-				});
 			}
 		} catch (error) {
 			console.log(error);
 		}
-		
 	};
 
 	useEffect(() => {
-		let timerOut= setTimeout(() =>{
+		let timerOut = setTimeout(() => {
 			getMovies(`${API_URL}&i=${id}`);
-		},1000);
-		return () => clearTimeout(timerOut)
+		}, 1000);
+		return () => clearTimeout(timerOut);
 	}, [id]);
 
-	if(isLoading){
-        return(
-            <div className="movie-section">
-                <div className='loading'>Loading...</div>
-            </div>
-        );
-    }
+	if (isLoading) {
+		return (
+			<div className="movie-section">
+				<div className="loading">Loading...</div>
+			</div>
+		);
+	}
 	return (
-		<>
-			<div>Our Single Movie {id}</div>
-		</>
+		<section className="movie-section">
+			<div className="movie-card">
+				<figure>
+					<img src={movie.Poster} alt="" />
+				</figure>
+				<div className="card-content">
+					<p className="title">{movie.Title}</p>
+					<p className=""></p>
+					<p className="card-text">{movie.Released}</p>
+					<p className="card-text">{movie.Genre}</p>
+					<p className="card-text">{movie.imdbRating} / 10</p>
+					<p className="card-text">{movie.Country}</p>
+					<NavLink to="/" className="back-btn">
+						Go Back
+					</NavLink>
+				</div>
+			</div>
+		</section>
 	);
 };
 
