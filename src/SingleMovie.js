a2656ada39670d6023d6649ddee03dd914b3e3c3
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-08-21 00:16:27 +0530

    inner card

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -4,8 +4,35 @@ import { useParams } from "react-router-dom";
 const SingleMovie = () => {
 	const { id } = useParams();
 	const [isLoading, setIsLoading] = useState(true);
-	const [isError, setIsError] = useState([]);
-	const [query, setQuery] = useState("titanic");
+	const [isError, setIsError] = useState("");
+
+	const getMovies= async=(url)=>{
+		setIsLoading(true);
+		try{
+			const res = await fetch(url);
+			const data = await res.json();
+			console.log(data);
+			if (data.Response === "True") {
+				setIsLoading(false);
+				setMovie(data.Search);
+			} else {
+				setIsError({
+					show: true,
+					msg: data.Error,
+				});
+			}
+		} catch (error) {
+			console.log(error);
+		}
+		
+	};
+
+	useEffect(() => {
+		let timerOut= setTimeout(() =>{
+			getMovies(`${API_URL}`);
+		},1000);
+		return () => clearTimeout(timerout)
+	}, [query]);
 	return (
 		<>
 			<div>Our Single Movie {id}</div>
