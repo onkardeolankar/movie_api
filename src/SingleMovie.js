f726cb2d43d25f730fcfb3ecc83db51f17f56171
onkardeolankar1996@gmail.com <codewithkeeda@gmail.com>
2022-08-21 00:38:06 +0530

    imports updated

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import { API_URL } from "./context";
 
 const SingleMovie = () => {
